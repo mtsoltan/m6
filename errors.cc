2bed8cfecb6b8b39c3ba97f62099544441437fbf
mtsoltan <mahmoud.t.soltan@gmail.com>
2021-08-12 11:43:52 +0200

    Fixed errors to support longer messages, and added an error.

--- errors.cc
+++ errors.cc
@@ -1,10 +1,11 @@
 #include <errors.h>
 
-const char errors[ERR_COUNT + 1][100] = {
+const char errors[ERR_COUNT + 1][MAX_ERR_SIZE] = {
         "",
         "[ERROR] Input file stream failed to read the file %s.",
         "[ERROR] Wrong number of arguments. Expected only one argument, the file name to interpret.",
         "[ERROR] A syntax error has been found while tokenizing.",
         "[ERROR] The size of the operator needs to be between 1 and 4, or 0 for checking all operators.",
         "[ERROR] The operator does not start with a punctuation yet we somehow made it to process_symbol.",
+        "[ERROR] Please set the token iterator to some start operator before calling find_end_of_start_operator.",
 };
--- include/errors.h
+++ include/errors.h
@@ -2,17 +2,19 @@
 #define M6_ERRORS_H
 
 
-#define ERR_COUNT 5
+#define ERR_COUNT 6
+#define MAX_ERR_SIZE 200
 
 #define ERR_IFSTREAM_FAILED         1
 #define ERR_INVALID_ARGC            2
 #define ERR_TOKENIZING_SYNTAX_ERROR 3
 #define ERR_OPERATOR_INVALID_SIZE   4
 #define ERR_OPERATOR_INVALID_PUNC   5
+#define ERR_INVALID_START_OPERATOR  6
 
 
 // TODO: https://github.com/mtsoltan/m6/issues/16
-extern const char errors[ERR_COUNT + 1][100];
+extern const char errors[ERR_COUNT + 1][MAX_ERR_SIZE];
 
 
 #endif
